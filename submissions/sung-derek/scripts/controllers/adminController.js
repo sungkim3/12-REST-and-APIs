6802270d0b9fbb504ebe4bcc5c09559b526cdcbc
sungkim3 <sng.kim103@gmail.com>
2016-05-24 12:44:37 -0700

    initiates starter-code for pair program assignment

---
+++ submissions/sung-derek/scripts/controllers/adminController.js
@@ -0,0 +1,9 @@
+(function(module) {
+  var adminController = {
+    index: function() {
+      Article.fetchAll(articleView.initAdminPage);
+      $('#blog-stats').show().siblings().hide();
+    }
+  };
+  module.adminController = adminController;
+})(window);
